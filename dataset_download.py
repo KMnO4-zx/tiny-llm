71a090cdd2bc79f467c1c780c366eddd87a129bc
KMnO4-zx <1021385881@qq.com>
2025-02-26 15:33:46 +0800

    Add dataset download

--- dataset_download.py
+++ dataset_download.py
@@ -1,49 +1,41 @@
 import os
+import json
+from tqdm import tqdm
 
-# 设置环境变量
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 # 下载预训练数据集
-for i in range(0, 17):
-    os.system(f'huggingface-cli download --repo-type dataset --resume-download Skywork/SkyPile-150B data/2020-40_zh_head_{str(i).zfill(4)}.jsonl --local-dir Skywork')
+os.system("modelscope download --dataset ddzhu123/seq-monkey mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2 --local_dir your_local_dir")
+# 解压预训练数据集
+os.system("tar -xvf your_local_dir/mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2")
 
-# 下载 SFT 数据集
+# 下载SFT数据集
 os.system(f'huggingface-cli download --repo-type dataset --resume-download BelleGroup/train_3.5M_CN --local-dir BelleGroup')
 
 
-# 两个数据集下载之后 都需要做一定的处理
 
-# 1. SkyPile-150B 数据集
-"""
-import json
-from tqdm import tqdm
-import os
-
-
+# 1 处理预训练数据
 def split_text(text, chunk_size=512):
+    """将文本按指定长度切分成块"""
     return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
 
+input_file = 'mobvoi_seq_monkey_general_open_corpus.jsonl'
 
-paths = os.listdir('data')
+with open('seq_monkey_datawhale.jsonl', 'a', encoding='utf-8') as pretrain:
+    with open(input_file, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+        for line in tqdm(data, desc=f"Processing lines in {input_file}", leave=False):  # 添加行级别的进度条
+            line = json.loads(line)
+            text = line['text']
+            chunks = split_text(text)
+            for chunk in chunks:
+                pretrain.write(json.dumps({'text': chunk}, ensure_ascii=False) + '\n')
 
-with open('pretrain.jsonl', 'a', encoding='utf-8') as pretrain:
-    for input_file in tqdm(paths, desc="Processing files"):  # 添加文件级别的进度条
-        with open('data/' + input_file, 'r', encoding='utf-8') as f:
-            data = f.readlines()
-            for line in tqdm(data, desc=f"Processing lines in {input_file}", leave=False):  # 添加行级别的进度条
-                line = json.loads(line)
-                text = line['text']
-                chunks = split_text(text)
-                for chunk in chunks:
-                    pretrain.write(json.dumps({'text': chunk}, ensure_ascii=False) + '\n')
-"""
-# 2. SFT 数据集
-# 将数据集转换为常用的格式
-"""
-import json
-from tqdm import tqdm
+# 2 处理SFT数据
 
 def convert_message(data):
+    """
+    将原始数据转换为标准格式
+    """
     message = [
         {"role": "system", "content": "你是一个AI助手"},
     ]
@@ -55,10 +47,9 @@ def convert_message(data):
     return message
 
 with open('BelleGroup_sft.jsonl', 'a', encoding='utf-8') as sft:
-    with open('train_3.5M_CN.json', 'r') as f:
+    with open('BelleGroup/train_3.5M_CN.json', 'r') as f:
         data = f.readlines()
         for item in tqdm(data, desc="Processing", unit="lines"):
             item = json.loads(item)
             message = convert_message(item['conversations'])
             sft.write(json.dumps(message, ensure_ascii=False) + '\n')
-"""
